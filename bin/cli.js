5499749d5b3c50f3cd20c64c32298aae877cd607
Nalinda Karunaratna <NalindaKarunaratna@gmail.com>
2016-07-06 22:18:12 +1000

    fixed few linting issues

--- bin/cli.js
+++ bin/cli.js
@@ -15,9 +15,8 @@ program
     .arguments('<file>')
     .option('-u, --username <username>', 'The user to authenticate as')
     .option('-p, --password <password>', 'The user\'s password')
-    .action(function(file) {
-        console.log('user: %s pass: %s file: %s',
-        program.username, program.password, file);
+    .action(function (file) {
+        console.log('user: %s pass: %s file: %s', program.username, program.password, file);
     })
     .parse(process.argv);
 
@@ -26,8 +25,7 @@ const reservedOptions = _.pick(args, 'cwd');
 
 const action = {
     name: args._,
-    options: _.omit(args, _.concat(['_'],
-    reservedOptions.keys))
+    options: _.omit(args, _.concat(['_'], reservedOptions.keys))
 };
 
 const envContext = new EnvContext({
