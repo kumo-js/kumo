113b55ed7982068f9548732ec25ca11b5c787091
Asheesh Karwal <asheesh.karwal@myob.com>
2016-07-06 18:26:14 +1000

    Uses static create for instantiating instances

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,5 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
+'use strict';
 
 const fs = require('fs');
 const _ = require('lodash');
@@ -10,7 +11,7 @@ const CommandFactory = require('../lib/command-factory');
 const args = minimist(process.argv.slice(2));
 const reservedOptions = _.pick(args, 'cwd');
 const action = {name: args._, options: _.omit(args, _.concat(['_'], reservedOptions.keys))};
-
 const envContext = new EnvContext({fs: fs, fileFinder: new FileFinder(), options: reservedOptions});
 const commandFactory = new CommandFactory({envContext: envContext});
+
 commandFactory.createCommand(action).execute();
