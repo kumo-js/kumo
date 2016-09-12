14c95f7d0e2085a4a79f5a7517076ba5c20e3ab5
Asheesh Karwal <asheesh.karwal@myob.com>
2016-09-12 12:57:15 +1000

    Creates context before executing command

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,23 @@
 #!/usr/bin/env node
 'use strict';
 
-const Promise = require('bluebird');
 const ConsoleLogger = require('../lib/console-logger');
+const ContextFactory = require('../lib/context-factory');
+const FileFinder = require('../lib/file-finder');
+const JsonCompatibleFileReader = require('../lib/json-compatible-file-reader');
 const KumoArgsParser = require('../lib/kumo-args-parser');
 const KumoCommandFactory = require('../lib/kumo-command-factory');
 
 const args = new KumoArgsParser().parse(process.argv);
 const logger = new ConsoleLogger({verbose: args.globalOptions.verbose});
+const fileFinder = new FileFinder();
+const fileReader = new JsonCompatibleFileReader();
+const contextFactory = new ContextFactory({fileFinder, fileReader});
 const commandFactory = new KumoCommandFactory({logger});
 
-Promise.resolve()
-    .then(() => commandFactory.createCommand(args).execute())
+contextFactory.createContext(args)
+    .then(context => commandFactory.createCommand(context))
+    .then(command => command.execute())
     .catch(err => {
         logger.error('Operation failed!', err);
         process.exit(1);
